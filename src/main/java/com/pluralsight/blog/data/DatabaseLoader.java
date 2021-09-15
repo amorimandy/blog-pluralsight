19e869006f8566893e28b89e83444fe7d96ff1a2
Andressa Amorim <dev.andymal@gmail.com>
2021-09-15 14:54:13 -0300

    Module 2 - Create the Post Repository and Save Data

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -21,7 +21,11 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
-    public DatabaseLoader() {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
     @Override
@@ -30,9 +34,13 @@ public class DatabaseLoader implements ApplicationRunner {
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
 
+
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
+
         });
+        postRepository.saveAll(randomPosts);
     }
+
 }
